6741eb56b4ce65c57256a8795bca012cb03bd026
Venus Bastidas <venus.bastidas.g@gmail.com>
2022-04-11 19:24:35 -0400

    Actualización

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+var tooltipTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  );
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+  });
+
+  $(function () {
+    $(document).scroll(function () {
+    var $nav = $(".navbar");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+    });
